38917a7c1adcc3eb026b5093de83370aab340217
Comentry <2055998373@qq.com>
2024-04-26 19:35:51 +0800

    rm redundancy

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -6,7 +6,7 @@ public class LinkedListDeque<T> implements Iterable<T>, Deque<T> {
     private Node sentinel;
     private int size;
 
-    private class Node {
+    class Node {
         private T item;
         private Node prev;
         private Node next;
@@ -117,7 +117,7 @@ public class LinkedListDeque<T> implements Iterable<T>, Deque<T> {
         return getRecursive(node.next, index - 1);
     }
 
-    private class LinkedListDequeIterator implements Iterator<T> {
+    class LinkedListDequeIterator implements Iterator<T> {
 
         private Node p;
 
